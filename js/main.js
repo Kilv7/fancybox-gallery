5bbbe044b47c4f1d76320f2f2b94025a791f2a73
Ilona Kostiuk <kilv.404@gmail.com>
2025-01-06 16:49:13 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+Fancybox.bind("[data-fancybox='gallery']", {
+  Thumbs: {
+    autoStart: true, // Відкрити мініатюри автоматично
+  },
+  Toolbar: {
+    display: ["close"], // Показати лише кнопку "Закрити"
+  },
+  Video: {
+    autoStart: true, // Автоматичний старт відео
+    ratio: 16 / 9, // Пропорція для відео (налаштуйте, якщо потрібно)
+  },
+  Image: {
+    zoom: true, // Дозволяє масштабувати зображення
+  },
+  on: {
+    initSlide: (fancybox, slide) => {
+      // Налаштування стилів для відео
+      const video = slide.$el.querySelector("video");
+      if (video) {
+        video.style.width = "100%";
+        video.style.height = "100%";
+        video.style.objectFit = "cover"; // Прибирає чорні краї
+      }
+    },
+  },
+});
+// Робота з модальним вікном
+const modal = document.getElementById('modal');
+const openModal = document.getElementById('openModal');
+const closeModal = document.querySelector('.close');
+
+const toggleModal = (action) => {
+  modal.classList[action]('show');
+};
+
+openModal.addEventListener('click', () => toggleModal('add'));
+closeModal.addEventListener('click', () => toggleModal('remove'));
+window.addEventListener('click', (event) => {
+  if (event.target === modal) toggleModal('remove');
+});
+
+
+
+
+
+
+
+
